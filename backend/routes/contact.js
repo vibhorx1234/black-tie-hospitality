406613a57b0cf4b586a4faae327473601e5f58c5
Hardik Singla <07hardiksingla@gmail.com>
2026-05-24 15:08:13 +0530

    cors

--- backend/routes/contact.js
+++ backend/routes/contact.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const nodemailer = require('nodemailer');
+const cors = require("cors");
 
+app.use(cors());
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
