635798c284b8f32bb8968cff15f1758b71d20501
Hardik Singla <07hardiksingla@gmail.com>
2026-05-24 16:16:33 +0530

    cors 3

--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,7 @@ const contactRoute = require('./routes/contact');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({
-  origin: "*",
-  methods: ['GET', 'POST'],
-}));
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
